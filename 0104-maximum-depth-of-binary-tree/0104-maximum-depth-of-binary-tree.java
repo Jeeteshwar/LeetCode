8191172ce6418503007b211b66cb60eeb63102ae
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2024-07-06 23:00:08 +0530

    Time: 0 ms (100%), Space: 42.3 MB (64.42%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,12 +15,24 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null) return 0; // checking if the tree is null
+        if(root == null){ // if the tree is empty
+            return 0;
+        }
 
+        if(root.left == null && root.right == null){ //if both leftsub tree and right subtree is empty return only root node
+            return 1;
+        }
 
-        //calling recursion to find the maximum value using math.max of both subtree recursion 
-        //also adding 1 for the root node
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        if(root.left == null){ // if left tree if empty , return depth of right subtree
+            return 1 + maxDepth(root.right);
+        
+        }
+        if(root.right == null){  // if right tree if empty , return depth of left subtree
+            return 1 + maxDepth(root.left);
+        }
+
+        //IF BOTH SUBTREES ARE PRESENT THEN FIND MIN DEPTH USING MATH.MIN AND RECURSION
+        return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
 
         
     }
