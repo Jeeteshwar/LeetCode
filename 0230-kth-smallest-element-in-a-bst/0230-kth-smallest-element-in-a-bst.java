2b64b1c5dbdf516788112a656c712239056a9c0a
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2024-07-14 20:20:53 +0530

    Time: 0 ms (100%), Space: 44.2 MB (50.78%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -16,6 +16,7 @@
 class Solution {
     int ans = Integer.MAX_VALUE; // to keep the ans
     int count = 1; // initially at 1
+    boolean found = false; // initially false
 
     public int kthSmallest(TreeNode root, int k) {
        if(root == null) return 0; //if tree is null
@@ -27,6 +28,7 @@ class Solution {
 
        if(count == k ){ // if the current count matches the kth value
         ans = root.val; //assign the current node value into the ans variable
+        found = true; //to flag that kth variable is found 
        }
        count++; // increment the count of count variable after each visit
 
