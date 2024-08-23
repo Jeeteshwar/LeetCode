db0389cfce293cd9d1868893f013df25d2ff1a4f
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2024-08-23 20:56:07 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.1%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,36 +14,24 @@
  * }
  */
 class Solution {
-    // Helper method to perform preorder traversal and populate the result list
-    void preorder(TreeNode root, List<Integer> result, int level) {
-        // Base case: if the node is null, return
-        if (root == null) return;
+    void helper(TreeNode root , List<Integer> result , int level){
+        if(root == null) return;
 
-        // If the current level is equal to the size of the result list, 
-        // it means we are visiting this level for the first time, so add the root node's value
-        if (result.size() == level) {
+        if(result.size() == level) 
             result.add(root.val);
-        } else {
-            // Otherwise, update the value at the current level with the node's value
-            result.set(level, root.val);
+        else{
+            result.set(level , root.val);
         }
-        
-       
 
-        // Recur for the left subtree to handle cases where right child is missing (first becuse when we do the rightsub the result overwrite the old left sub tree values)
-        preorder(root.left, result, level + 1);
+        helper(root.left, result , level + 1);
+        helper(root.right, result , level + 1);
 
-         // Recur for the right subtree
-        preorder(root.right, result, level + 1);
     }
-    
-    // Method to return the right side view of the binary tree
     public List<Integer> rightSideView(TreeNode root) {
-        // Initialize the result list
         List<Integer> result = new ArrayList<>();
-        // Call the helper method to populate the result list
-        preorder(root, result, 0);
-        // Return the result list
+
+        helper(root , result , 0);
+
         return result;
     }
 }
