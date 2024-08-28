34cc91980fd51208e10a27d686916e40887dc68e
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2024-08-28 19:12:07 +0530

    Time: 28 ms (34.79%), Space: 74.9 MB (86.01%) - LeetHub

---
+++ 2035-count-sub-islands/2035-count-sub-islands.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    private final int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}; // down, up, right, left
+
+    // BFS
+    private boolean checkSubIsland(int[][] grid1, int[][] grid2, int i, int j) {
+        int m = grid1.length;
+        int n = grid1[0].length;
+
+        boolean result = true;
+
+        Queue<int[]> queue = new LinkedList<>(); // Queue to perform BFS
+        queue.offer(new int[]{i, j});
+        grid2[i][j] = -1; // mark visited
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+
+            if (grid1[x][y] != 1) { // grid1 must have 1 at the same coordinate
+                result = false;
+            }
+
+            for (int[] dir : directions) {
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n && grid2[newX][newY] == 1) {
+                    grid2[newX][newY] = -1; // mark visited
+                    queue.offer(new int[]{newX, newY});
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int subIslands = 0;
+        int m = grid2.length; // rows
+        int n = grid2[0].length; // cols
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid2[i][j] == 1 && checkSubIsland(grid1, grid2, i, j)) { // Found an island
+                    subIslands++;
+                }
+            }
+        }
+
+        return subIslands;
+    }
+}
