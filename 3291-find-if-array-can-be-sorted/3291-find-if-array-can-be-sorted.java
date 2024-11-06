d2c62724c4e619bc58a9c2c0f1f23cb0ff7ae43a
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2024-11-06 23:00:13 +0530

    Time: 3 ms (48.57%), Space: 44.8 MB (11.32%) - LeetHub

---
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canSortArray(int[] arr) {
+        int n = arr.length;
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i + 1 ; j < n ; j++ ){
+                if(arr[j] < arr[i]){
+                    if(Integer.bitCount(arr[i]) != Integer.bitCount(arr[j])){
+                        return false;
+                    }
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for(int k = 0 ; k < n-1 ; k++){
+            if(arr[k] > arr[k + 1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
