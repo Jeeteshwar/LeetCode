ecd592326bcf8c09a54b01510551e95f929b8e04
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2024-12-09 14:28:36 +0530

    Time: 3 ms (86.83%), Space: 98.7 MB (41.15%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.java
+++ 3427-special-array-ii/3427-special-array-ii.java
@@ -8,26 +8,27 @@ class Solution {
         cumSum[0] = 0;
 
         for(int i = 1 ; i < n ; i++){
-            if(nums[i] % 2 == nums[i - 1] % 2){
-                cumSum[i] = cumSum[i - 1] + 1;
+            if(nums[i] % 2 == nums[i - 1] % 2){ // true if both are same partity (odd , odd) 
+                                                // false if both are different parity(odd,even)
+                cumSum[i] = cumSum[i - 1] + 1; // if true then previous value + 1
             }else{
-                cumSum[i] = cumSum[i  - 1];
+                cumSum[i] = cumSum[i  - 1]; // else carry the same previous value 
 
             }
         }
-        boolean [] res =  new boolean [m];
+        boolean [] res =  new boolean [m]; // for storing the boolean values
 
         for(int i = 0 ; i < m ; i++){
-            int start =  queries[i][0];
+            int start =  queries[i][0]; // 2d arr entry 
             int end =  queries[i][1];
 
-            if(cumSum[end] - cumSum[start] == 0){
+            if(cumSum[end] - cumSum[start] == 0){ // if end - start == 0 add true in the bool arr
                 res[i] =  true;
             }
             else{
-                res[i] = false;
+                res[i] = false; // else add false 
             }
         }
-        return res;
+        return res; // return the final result
     }
 }
