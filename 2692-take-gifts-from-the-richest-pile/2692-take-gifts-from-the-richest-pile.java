6091ce9b5d98d080c38add8603f93e939ee95b97
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2024-12-12 14:24:40 +0530

    Time: 5 ms (89.83%), Space: 42.4 MB (51.8%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
@@ -1,21 +1,16 @@
 class Solution {
     public long pickGifts(int[] gifts, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        for(int val : gifts){
-            pq.add(val);
-
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // max heap
+        for(int val : gifts){ // adding the elements from arr into the maxheap
+            pq.add(val); // function call of adding
         }
-
-        for(int i = 0 ; i < k ; i++){
-            int x  = pq.poll();
-
-            pq.add((int) Math.sqrt(x));
+        for(int i = 0 ; i < k ; i++){ // loop till the k valuie
+            int x  = pq.poll(); // getting the max elements from the maxHeap everytime
+            pq.add((int) Math.sqrt(x)); // adding the square value of current max into maxHeap
         }
-
-        long sum = 0;
-        for(int val : pq){
-            sum+=val;
+        long sum = 0; // sum that will get return as remaining
+        for(int val : pq){ 
+            sum+=val; // left sum left
         }
         return sum;
     }
