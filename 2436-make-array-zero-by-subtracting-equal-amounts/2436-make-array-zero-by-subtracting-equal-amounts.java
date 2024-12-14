8a70b74545ee770ba03faf5a694d684df97e3eb2
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2024-12-14 23:03:09 +0530

    Time: 1 ms (94.88%), Space: 41.5 MB (29.42%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,21 +1,21 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue <>();
+        PriorityQueue<Integer> pq = new PriorityQueue <>(); //min heap
         int n = nums.length;
         int count = 0;
 
-        for(int num : nums){
-            pq.offer(num);
+        for(int num : nums){ 
+            pq.offer(num); // adding into heap
         }
-        int temp = -1;
+        int temp = -1; // to check duplicate occurance
         for(int i = 0 ; i < n ; i++){
-            int x  = pq.poll();
+            int x  = pq.poll(); // adding smallest element into the x
 
-            if(x == 0 || x == temp)
+            if(x == 0 || x == temp) // if true then move to next i element ( basically checking if curr element is 0 or like previous value(duplicate))
                 continue;
         
-        temp = x;
-        count++;
+        temp = x; // to keep the current passed element(basically checks for duplicate)
+        count++; // increment the result
         }
         return count;
 
