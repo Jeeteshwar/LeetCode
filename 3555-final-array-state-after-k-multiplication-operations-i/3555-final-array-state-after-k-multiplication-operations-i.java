8f6245a91234abb5efcd1ad1c90549ef2714527c
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2024-12-16 18:43:12 +0530

    Time: 1 ms (100%), Space: 45.1 MB (12.28%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -1,18 +1,23 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        int n = nums.length;
+        int n = nums.length; // Get the size of the array
 
-        for(int i = 0 ; i < k ; i++){
-            int minIdx = 0;
+        // Perform the operation `k` times
+        for (int i = 0; i < k; i++) {
+            int minIdx = 0; // Initialize the index of the smallest element to 0
 
-            for(int j = 0 ; j < n ; j++){
-                if(nums[j] < nums[minIdx]){
-                    minIdx = j;
+            // Find the index of the smallest element in the array
+            for (int j = 0; j < n; j++) {
+                if (nums[j] < nums[minIdx]) { // If nums[j] is smaller than the current smallest
+                    minIdx = j; // Update the index of the smallest element
                 }
-
             }
+
+            // Multiply the smallest element by the multiplier
             nums[minIdx] *= multiplier;
         }
+
+        // Return the modified array after `k` operations
         return nums;
     }
 }
