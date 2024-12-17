153a2ea108147d06a4806ce7a92ce6872821813a
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2024-12-17 22:16:24 +0530

    Time: 14 ms (57.93%), Space: 49.2 MB (12.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -6,7 +6,9 @@ class Solution {
             map.put(nums[i],map.getOrDefault(nums[i],0) + 1);
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a , b) -> map.get(a) - map.get(b));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a , b) -> map.get(a) - map.get(b)); // sorting the pq with less frequent at top
+                                                                                            // if want most frequent at top 
+                                                                                            // ((a,b) -> map.get(b) - map.get(a))
 
         for(int num : map.keySet()){
             pq.add(num);
