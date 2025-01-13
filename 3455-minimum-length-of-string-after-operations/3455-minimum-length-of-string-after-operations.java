d611d458edddaa155c393efccf8e98d1779f9c77
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2025-01-14 02:03:50 +0530

    Time: 9 ms (92%), Space: 46.6 MB (26.67%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -4,23 +4,23 @@ class Solution {
         int ans = 0;
 
         int arr [] = new int [26];
-
+        // FREQUENCY OF ALL ALPHABET
         for(int i = 0 ; i < n ; i++){
             arr[s.charAt(i) - 'a']++;
         }
 
         for(int i = 0 ; i < 26 ; i++){
-            if(arr[i] == 0){
+            if(arr[i] == 0){  // IF ANY FREQUQNCY OF ALPHABET IS 0 SKIP 
                 continue;
             }
-            if(arr[i] % 2 != 0 ){
+            if(arr[i] % 2 != 0 ){ // IF FREQUENCY OF ALPHABET IS EVEN +1
                 ans++;
             }
-            else{
+            else{ //IF OFF +2
                 ans+=2;
             }
             
         }
-        return ans;
+        return ans; //RETURN ANS
     }
 }
