d7f9b588a148eea3b3657038e054d13b6ee81d9a
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2025-02-07 08:58:31 +0530

    Time: 1 ms (50.52%), Space: 45 MB (42.62%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,34 +1,30 @@
 class Solution {
-    // Helper method to swap elements at indices i and j in the array nums
-    private void swap(int[] nums, int i, int j) {
+    public void swap(int[] nums , int i , int j){
         int temp = nums[i];
         nums[i] = nums[j];
-        nums[j] = temp;
+        nums[j] =  temp;
+
     }
 
-    // Method to sort array such that all even elements precede all odd elements
     public int[] sortArrayByParity(int[] nums) {
-        int n = nums.length; // Get the length of the array
-
-        int i = 0; // Initialize pointer i to start of the array
-        int j = n - 1; // Initialize pointer j to end of the array
-
-        // Loop until the two pointers meet
-        while (i < j) {
-            // If the element at i is even, move the i pointer to the right
-            if (nums[i] % 2 == 0)
+        int n = nums.length;
+        int i = 0;
+        int j = n - 1;
+        while(i < j){
+            //even satisfied
+            if(nums[i] % 2 == 0){
                 i++;
-            // If the element at j is odd, move the j pointer to the left
-            else if (nums[j] % 2 != 0) 
-                j--;
-            // If nums[i] is odd and nums[j] is even, swap them
-             else {
-                swap(nums, i, j);
-                i++;
+            } 
+            // odd satisfied
+            else if(nums[j] % 2 != 0){
                 j--;
             }
-            
+            // swap krna ha
+            else{
+                swap(nums, j, i);
+                
+            }
         }
-        return nums; // Return the sorted array
+        return nums;
     }
 }
