c936f65c99ccd219ce9e7c05c9b67c6c323ea7b0
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2025-06-13 23:28:05 +0530

    Time: 17 ms (78%), Space: 56.1 MB (63%) - LeetHub

---
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minimizeMax(int[] nums, int p) {
+        Arrays.sort(nums);
+        int left = 0, right = nums[nums.length - 1] - nums[0];
+        int result = right;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (canFormPairs(nums, p, mid)) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return result;
+    }
+
+    private boolean canFormPairs(int[] nums, int p, int maxDiff) {
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] <= maxDiff) {
+                count++;
+                i++; // skip the next one to avoid reuse
+            }
+        }
+        return count >= p;
+    }
+}
