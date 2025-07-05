09514d1e288952b6020146e04f1a6ad35cfa1cd6
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2025-07-05 22:07:14 +0530

    Time: 1 ms (99.94%), Space: 43.5 MB (43.8%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
@@ -1,17 +1,28 @@
 class Solution {
     public int findLucky(int[] arr) {
-        int n = arr.length;
+        int n = arr.length;  // Store the size of the input array (not strictly necessary in this problem)
 
-        int [] Total =  new int [501];
-        for(int num : arr){
+        // Step 1: Create a frequency array to count how many times each number appears in 'arr'
+        // The problem says arr[i] is in the range [1, 500], so we make an array of size 501
+        int[] Total = new int[501];  // Index 0 will be unused
+
+        // Step 2: Fill the frequency array
+        // For each number in the input array, increase its count at the corresponding index
+        for (int num : arr) {
             Total[num]++;
         }
 
-        for(int i = 500 ; i > 0 ; i--){
-            if(i == Total[i]){
-                return i;
+        // Step 3: Find the largest "lucky" number
+        // A lucky number is a number whose value is equal to its frequency
+        // We loop from 500 down to 1 to get the largest lucky number first
+        for (int i = 500; i > 0; i--) {
+            // If the number 'i' appears exactly 'i' times, it is a lucky number
+            if (i == Total[i]) {
+                return i;  // Return immediately once we find the largest such number
             }
         }
+
+        // Step 4: If no lucky number is found, return -1
         return -1;
     }
 }
