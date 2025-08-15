e46bb6770551e576d05896f3fef5c61b8f1913c3
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2025-08-15 16:26:48 +0530

    Time: 0 ms (100%), Space: 41.4 MB (8.89%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-           if(n < 1) return false; // If n is less than 1, it cannot be a power of four
-            if(n == 1) return true;  // If n is 1, it is 4^0, which is a power of four
+        if (n == 1) return true;      // base case: 4^0 = 1
+        if (n < 1) return false;      // negative numbers or 0 are not powers of 4
 
-        if(n % 4 ==0) return isPowerOfFour(n /4); // If n is divisible by 4, recursively check if n/4 is a power of four
-        
+        if (n % 4 == 0)               // if divisible by 4
+            return isPowerOfFour(n / 4); // reduce the problem
 
-        
-       return false;  // If none of the above conditions are met, n is not a power of four
+        return false;                 // if not divisible by 4, stop
     }
 }
