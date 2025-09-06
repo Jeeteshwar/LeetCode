44ed42b9e9af3ba1ac11bc31cab618c99c71c02c
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2025-09-06 21:41:28 +0530

    Time: 2 ms (94.01%), Space: 47 MB (21.6%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,11 +1,16 @@
 class Solution {
+    private int countOnes(int n){
+        int count = 0;
+        while(n != 0){
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
     public int[] countBits(int n) {
         int [] arr = new int [n + 1];
-        arr[0] = 0;
-        while(n != 0){
-            int ones = Integer.bitCount(n);
-            arr[n] = ones;
-            n--;
+        for(int i = 0 ; i <= n ; i++){
+            arr[i] = countOnes(i);
         }
         return arr;
     }
