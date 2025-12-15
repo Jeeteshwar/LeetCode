4dba855a7a6b4e0b45655b79f2ee82ca0f7defc6
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2025-12-16 00:44:17 +0530

    Time: 2 ms (100%), Space: 83.6 MB (68.89%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.java
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.java
@@ -2,20 +2,19 @@ class Solution {
     public long getDescentPeriods(int[] prices) {
         int n = prices.length;
         long allCount = 0;
-        int currCount = 1;  // Start with 1 because a single day is a descent period of length 1.
+        int currCount = 1;
 
-        for (int i = 1; i < n; i++) {
-            if (prices[i - 1] - prices[i] == 1) {
-                // We're continuing a descent.
+        for(int i = 1 ; i < n ; i++){
+             if (prices[i - 1] - prices[i] == 1) {
                 currCount++;
-            } else {
-                // Descent broke, reset current count to 1 for the new single-day period.
+            }
+            else{
                 currCount = 1;
             }
-            // Add the current streak length to the total count.
             allCount += currCount;
+        
         }
         
-        return allCount + 1 ;
+        return allCount + 1;
     }
-}
+}        
