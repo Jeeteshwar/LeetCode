25e8f168aba3cb36e49ad88c9eb2ab6bbb452c16
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2025-12-27 23:04:16 +0530

    Time: 5 ms (57.26%), Space: 44 MB (13.98%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -1,18 +1,18 @@
 class Solution {
     public int minOperations(String s) {
-        int startZero = 0;
-        int startOne = 0;
+        int startZero = 0; //010101
+        int startOne = 0;//101010
 
         for (int i = 0; i < s.length(); i++) {
             char curr = s.charAt(i);
 
-            if (i % 2 == 0) {
-                if (curr != '0') startZero++;
-                if (curr != '1') startOne++;
-            } else {
-                if (curr != '1') startZero++;
-                if (curr != '0') startOne++;
-            }
+            if (i % 2 == 0) { //even
+                if (curr == '0') startOne++; //expects a 0 at even idx -> so startZero++
+                else if (curr == '1') startZero++;  //expects a 1 at even idx -> so startOne++
+        } else { //odd
+                if (curr == '1') startOne++; //expects a 1 at odd idx -> so startZero++
+                else if (curr ==  '0') startZero++;  //expects a 0 at odd idx -> so startOne++
+            } 
         }
 
         return Math.min(startZero, startOne);
