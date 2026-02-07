a8152f1ca5210e643c635e8281e2987edf67261a
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2026-02-07 16:04:43 +0530

    Time: 19 ms (93.09%), Space: 48 MB (16.28%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        // Initialize the result (res) to track the number of deletions required
+        int res = 0;
+        // Initialize count to track the number of 'b' characters encountered
+        int count = 0;
+
+        // Iterate through each character in the string
+        for (char c : s.toCharArray()) {
+            // If the character is 'b', increment the count of 'b's
+            if (c == 'b') {
+                count++;
+            } 
+            // If the character is 'a' and there are 'b's encountered before it 
+            else if (c == 'a' && count != 0) {
+                // Increment the result since this 'a' is out of order
+                res++;
+                // Decrement the count, as we're effectively "removing" a 'b' by considering it for deletion
+                count--;
+            }
+        }
+        
+        // Return the total number of deletions required
+        return res;
+    }
+}
