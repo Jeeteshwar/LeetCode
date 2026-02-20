e547c0a7778c2b02b778c6b53ea863d6a6af03c9
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2026-02-20 23:33:40 +0530

    Time: 2 ms (100%), Space: 43.4 MB (48.34%) - LeetHub

---
+++ 0761-special-binary-string/0761-special-binary-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String makeLargestSpecial(String s) {
+        int count = 0;
+        int start = 0;
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') count++;
+            else count--;
+
+            if (count == 0) {
+                String inner = s.substring(start + 1, i);
+                list.add("1" + makeLargestSpecial(inner) + "0");
+                start = i + 1;
+            }
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        StringBuilder res = new StringBuilder();
+        for (String str : list) res.append(str);
+
+        return res.toString();
+    }
+}
