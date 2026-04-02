b79f493bdc11071565c2f704a35f13a52b425fe6
Jeeteshwar Shrivastava <84372784+Jeeteshwar@users.noreply.github.com>
2026-04-02 22:20:50 +0530

    Time: 2 ms (35.21%), Space: 46.2 MB (81.83%) - LeetHub

---
+++ 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minAbsoluteDifference(int[] nums) {
+        int n = nums.length;
+        int minAbs = Integer.MAX_VALUE;
+        for(int i = 0 ; i< n ; i++){
+           
+            for(int j = i + 1 ; j < n ; j++){
+                if(nums[i] == 1 && nums[j] == 2 ||nums[i] == 2 && nums[j] == 1){
+                    int currAbs =  Math.abs(i-j);
+                    minAbs = Math.min(minAbs , currAbs);
+                }
+            }
+
+            
+
+        }
+        return minAbs ==Integer.MAX_VALUE ? -1 : minAbs;
+    }
+}
